ef579e74eb2dce576943aec0f2ca9b70a4e693ed
Bspa7 <bpinedaareiza@worldbank.org>
2024-06-05 21:05:04 -0500

    Update Step3 - first estimations.R

--- R/Exploratory analysis/Step3 - first estimations.R
+++ R/Exploratory analysis/Step3 - first estimations.R
@@ -55,7 +55,8 @@ centrar_puntaje <- function(df) {
 }
 
 # Define a function to create all set of estimations
-run_estimations <- function(data, var_name, df_name) {
+# Define a function to create all set of estimations in cutoff=30
+run_estimations <- function(data, var_name, cutoff_var, df_name) {
   # Define time window
   years <- 2013:2019
   quarters <- 1:4
@@ -82,7 +83,7 @@ run_estimations <- function(data, var_name, df_name) {
         filter(year(quarterly_date) == year & quarter(quarterly_date) == qtr)
       
       # Make the estimate
-      est <- rdrobust(data_subset[[var_name]], data_subset$cutoff30, all=TRUE)
+      est <- rdrobust(data_subset[[var_name]], data_subset[[cutoff_var]], all=TRUE)
       summary(est)
       
       # Extract values corresponding to the robust estimate
@@ -157,9 +158,7 @@ main_pila         <- open_dataset(PILA_history_file) %>%
 
 # 3. Modified databases to estimations -----------------------------------------
 
-# 3.1. Databases to estimations (rdrobust)
-
-# RIPS: creating a monthly panel at personabasicaid-month level outcomes
+# 3.1. RIPS: panel at personabasicaid-quarter level outcomes -------------------
 rips_pbid <- main_rips %>%
   filter(COD_DPTO == 11 & COD_MPIO == 1) %>%
   categorize_age() %>%
@@ -211,45 +210,245 @@ rips_pbid <- rips_pbid %>%
   ) %>% 
   group_by(quarterly_date, personabasicaid) %>%  
   summarise(
-    puntaje   = max(puntaje),    
-    genero    = max(genero),
-    estrato_1 = max(estrato_1),
-    estrato_2 = max(estrato_2),
-    estrato_3 = max(estrato_3),
-    estrato_4 = max(estrato_4),
-    estrato_5 = max(estrato_5),
-    estrato_6 = max(estrato_6),
-    educacion = max(anios_educacion),
+    puntaje   = max(puntaje, na.rm = TRUE),    
+    genero    = max(genero,  na.rm = TRUE),
+    estrato_1 = max(estrato_1, na.rm = TRUE),
+    estrato_2 = max(estrato_2, na.rm = TRUE),
+    estrato_3 = max(estrato_3, na.rm = TRUE),
+    estrato_4 = max(estrato_4, na.rm = TRUE),
+    estrato_5 = max(estrato_5, na.rm = TRUE),
+    estrato_6 = max(estrato_6, na.rm = TRUE),
+    educacion = max(anios_educacion, na.rm = TRUE),
+    edad_RIPS = max(edad_RIPS, na.rm = TRUE),
     n_visitas = n(),
-    n_consultas         = sum(MODULE == "c"),
-    n_hospitalizaciones = sum(MODULE == "h"),
-    n_procedimientos    = sum(MODULE == "p"),
-    n_urgencias         = sum(MODULE == "u"),    
+    n_consultas         = sum(MODULE == "c", na.rm = TRUE),
+    n_hospitalizaciones = sum(MODULE == "h", na.rm = TRUE),
+    n_procedimientos    = sum(MODULE == "p", na.rm = TRUE),
+    n_urgencias         = sum(MODULE == "u", na.rm = TRUE),    
+    c_cancer = sum(if_else(MODULE == "c" & substr(DIAG_PRIN, 1, 1) == "C", 1, 0), na.rm = TRUE),
+    c_cardio = sum(if_else(MODULE == "c" & substr(DIAG_PRIN, 1, 1) == "I", 1, 0), na.rm = TRUE),    
+    c_preven = sum(if_else(MODULE == "c" & substr(DIAG_PRIN, 1, 1) == "Z" & substr(DIAG_PRIN, 2, 4) %in% sprintf("%03d", 0:139), 1, 0), na.rm = TRUE),
+    c_prenat = sum(if_else(MODULE == "c" & substr(DIAG_PRIN, 1, 1) == "Z" & substr(DIAG_PRIN, 2, 4) %in% sprintf("%03d", 300:392), 1, 0), na.rm = TRUE),  
+    c_respir = sum(if_else(MODULE == "c" & substr(DIAG_PRIN, 1, 1) == "J" & substr(DIAG_PRIN, 2, 4) %in% sprintf("%03d", 401:459), 1, 0), na.rm = TRUE)
+      )
+
+
+# ----
+
+# 3.2. PILA: panel at personabasicaid-quarter level outcomes -------------------
+
+pila_pbid <- main_pila %>%
+  filter(depto_cod == 11 & ciudad_cod == 1) %>%
+  mutate(
+    DATE = as.Date(DATE),
+    quarterly_date = floor_date(DATE, "quarter"),
+    year = year(DATE),
+    tipo_cotizante = case_when(
+      tipo_cotiz == 1 ~ 1,
+      tipo_cotiz == 3 ~ 2,
+      (tipo_cotiz != 1 & tipo_cotiz != 3) ~ 3,
+      is.na(tipo_cotiz) ~ 3),
+      genero    = if_else(sexomode == 1, 1, 0),
+      estrato_1 = if_else(estrato == 1, 1, 0),
+      estrato_2 = if_else(estrato == 2, 1, 0),    
+      estrato_3 = if_else(estrato == 3, 1, 0),
+      estrato_4 = if_else(estrato == 4, 1, 0),
+      estrato_5 = if_else(estrato == 5, 1, 0),
+      estrato_6 = if_else(estrato == 6, 1, 0),    
+      anios_educacion = case_when(
+        # Primaria
+        nivel == 3 & (grado == 0 | is.na(grado)) ~ 0,
+        nivel == 3 & grado == 1 ~ 1,
+        nivel == 3 & grado == 2 ~ 2,
+        nivel == 3 & grado == 3 ~ 3,
+        nivel == 3 & grado == 4 ~ 4,
+        nivel == 3 & grado == 5 ~ 5,
+        
+        # Secundaria
+        nivel == 2 & (grado == 0 | is.na(grado)) ~ 5,
+        nivel == 2 & (grado == 6 | grado == 1) ~ 6,
+        nivel == 2 & (grado == 7 | grado == 2) ~ 7,
+        nivel == 2 & (grado == 8 | grado == 3) ~ 8,
+        nivel == 2 & (grado == 9 | grado == 4) ~ 9,
+        nivel == 2 & (grado == 10 | grado == 5) ~ 10,
+        nivel == 2 & (grado == 11 | grado == 6) ~ 11,
+        
+        # Técnica o tecnología
+        nivel == 3 ~ 12,
+        
+        # Universitaria
+        nivel == 4 ~ 13,
+        
+        # Postgrado
+        nivel == 5 ~ 14,
+        
+        # Valor por defecto
+        TRUE ~ 0
+      )    
+  ) %>% 
+  group_by(quarterly_date, personabasicaid) %>%  
+  summarise(
+    year      = max(year, na.rm = TRUE),
+    yearmode  = max(yearmode, na.rm = TRUE),    
+    puntaje   = max(puntaje, na.rm = TRUE),    
+    genero    = max(genero, na.rm = TRUE),
+    estrato_1 = max(estrato_1, na.rm = TRUE),
+    estrato_2 = max(estrato_2, na.rm = TRUE),
+    estrato_3 = max(estrato_3, na.rm = TRUE),
+    estrato_4 = max(estrato_4, na.rm = TRUE),
+    estrato_5 = max(estrato_5, na.rm = TRUE),
+    estrato_6 = max(estrato_6, na.rm = TRUE),
+    educacion = max(anios_educacion, na.rm = TRUE),
+    tipo_cotizante  = max(tipo_cotizante, na.rm = TRUE),
+    n_registros = n(),
+    ibc_salud     = max(ibc_salud, na.rm = TRUE),
+    sal_dias_cot  = sum(sal_dias_cot, na.rm = TRUE)
   )
 
-# --
-
-
-rips_pbid <- rips_pbid %>%
-  mutate(quarterly_date = as.Date(quarterly_date))
-
-rips_pbid <- rips_pbid %>% 
+pila_pbid <- pila_pbid %>% 
   collect
 
-rips_pbid <- rips_pbid %>% 
-  centrar_puntaje()
+gc()
 
+pila_pbid <- pila_pbid %>% 
+  mutate(
+    year = as.numeric(year),  
+    yearmode = as.numeric(yearmode),
+    edad_PILA = year - yearmode,
+  ) %>% 
+  categorize_age_pila()
 
-
-
+# ----
 
 
 # 4. ESTIMATES -----------------------------------------------------------------
+
+rips_est <- rips_pbid %>%
+  mutate(quarterly_date = as.Date(quarterly_date)) %>% 
+  centrar_puntaje() %>% 
+  collect
+
+pila_est <- pila_pbid %>%
+  mutate(quarterly_date = as.Date(quarterly_date)) %>% 
+  centrar_puntaje()
+
+rm(rips_pbid, pila_pbid)
+gc()
+
+pila_est <- pila_est %>% 
+  mutate(
+    asalariado  = if_else(tipo_cotizante == 1, 1, 0)
+  )
+
+# RIPS
+
+run_estimations(rips_est, "genero", "cutoff30", "rips30_genero")
+run_estimations(rips_est, "estrato_1", "cutoff30", "rips30_estrato1")
+run_estimations(rips_est, "estrato_2", "cutoff30", "rips30_estrato2")
+run_estimations(rips_est, "estrato_3", "cutoff30", "rips30_estrato3")
+run_estimations(rips_est, "estrato_4", "cutoff30", "rips30_estrato4")
+run_estimations(rips_est, "estrato_5", "cutoff30", "rips30_estrato5")
+run_estimations(rips_est, "estrato_6", "cutoff30", "rips30_estrato6")
+run_estimations(rips_est, "educacion", "cutoff30", "rips30_estrato7")
+run_estimations(rips_est, "edad_RIPS", "cutoff30", "rips30_edad")
+run_estimations(rips_est, "n_visitas", "cutoff30", "rips30_visitas")
+run_estimations(rips_est, "c_preven" , "cutoff30", "rips30_c_prev")
+run_estimations(rips_est, "c_prenat" , "cutoff30", "rips30_c_pren")
+run_estimations(rips_est, "c_cancer" , "cutoff30", "rips30_c_cancer")
+run_estimations(rips_est, "c_cardio" , "cutoff30", "rips30_c_cardio")
+run_estimations(rips_est, "c_respir" , "cutoff30", "rips30_c_respir")
+run_estimations(rips_est, "n_consultas", "cutoff30", "rips30_consultas")
+run_estimations(rips_est, "n_hospitalizaciones", "cutoff30", "rips30_hosp")
+run_estimations(rips_est, "n_procedimientos", "cutoff30", "rips30_proc")
+run_estimations(rips_est, "n_urgencias", "cutoff30", "rips30_urgencias")
+
+
+run_estimations(rips_est, "genero", "cutoff40", "rips40_genero")
+run_estimations(rips_est, "estrato_1", "cutoff40", "rips40_estrato1")
+run_estimations(rips_est, "estrato_2", "cutoff40", "rips40_estrato2")
+run_estimations(rips_est, "estrato_3", "cutoff40", "rips40_estrato3")
+run_estimations(rips_est, "estrato_4", "cutoff40", "rips40_estrato4")
+run_estimations(rips_est, "estrato_5", "cutoff40", "rips40_estrato5")
+run_estimations(rips_est, "estrato_6", "cutoff40", "rips40_estrato6")
+run_estimations(rips_est, "educacion", "cutoff40", "rips40_estrato7")
+run_estimations(rips_est, "edad_RIPS", "cutoff40", "rips40_edad")
+run_estimations(rips_est, "n_visitas", "cutoff40", "rips40_visitas")
+run_estimations(rips_est, "c_preven" , "cutoff40", "rips40_c_prev")
+run_estimations(rips_est, "c_prenat" , "cutoff40", "rips40_c_pren")
+run_estimations(rips_est, "c_cancer" , "cutoff40", "rips40_c_cancer")
+run_estimations(rips_est, "c_cardio" , "cutoff40", "rips40_c_cardio")
+run_estimations(rips_est, "c_respir" , "cutoff40", "rips40_c_respir")
+run_estimations(rips_est, "n_consultas", "cutoff40", "rips40_consultas")
+run_estimations(rips_est, "n_hospitalizaciones", "cutoff40", "rips40_hosp")
+run_estimations(rips_est, "n_procedimientos", "cutoff40", "rips40_proc")
+run_estimations(rips_est, "n_urgencias", "cutoff40", "rips40_urgencias")
+
+
+# PILA
+run_estimations(pila_est, "genero", "cutoff30", "pila30_genero")
+run_estimations(pila_est, "estrato_1", "cutoff30", "pila30_estrato1")
+run_estimations(pila_est, "estrato_2", "cutoff30", "pila30_estrato2")
+run_estimations(pila_est, "estrato_3", "cutoff30", "pila30_estrato3")
+run_estimations(pila_est, "estrato_4", "cutoff30", "pila30_estrato4")
+run_estimations(pila_est, "estrato_5", "cutoff30", "pila30_estrato5")
+run_estimations(pila_est, "estrato_6", "cutoff30", "pila30_estrato6")
+run_estimations(pila_est, "educacion", "cutoff30", "pila30_estrato7")
+run_estimations(pila_est, "tipo_cotizante", "cutoff30", "pila30_tipo_czte")
+run_estimations(pila_est, "n_registros",    "cutoff30", "pila30_registros")
+run_estimations(pila_est, "ibc_salud" ,     "cutoff30", "pila30_ibc_salud")
+run_estimations(pila_est, "sal_dias_cot" ,  "cutoff30", "pila30_sal_dias_cot")
+run_estimations(pila_est, "edad_PILA" ,     "cutoff30", "pila30_edad_pila")
+
+
+run_estimations(pila_est, "genero", "cutoff40", "pila40_genero")
+run_estimations(pila_est, "estrato_1", "cutoff40", "pila40_estrato1")
+run_estimations(pila_est, "estrato_2", "cutoff40", "pila40_estrato2")
+run_estimations(pila_est, "estrato_3", "cutoff40", "pila40_estrato3")
+run_estimations(pila_est, "estrato_4", "cutoff40", "pila40_estrato4")
+run_estimations(pila_est, "estrato_5", "cutoff40", "pila40_estrato5")
+run_estimations(pila_est, "estrato_6", "cutoff40", "pila40_estrato6")
+run_estimations(pila_est, "educacion", "cutoff40", "pila40_estrato7")
+run_estimations(pila_est, "n_registros",    "cutoff40", "pila40_registros")
+run_estimations(pila_est, "ibc_salud" ,     "cutoff40", "pila40_ibc_salud")
+run_estimations(pila_est, "sal_dias_cot" ,  "cutoff40", "pila40_sal_dias_cot")
+run_estimations(pila_est, "edad_PILA" ,     "cutoff40", "pila40_edad_pila")
+run_estimations(pila_est, "asalariado", "cutoff40", "pila40_asalariado")
+
+# ----
+
+print(Sys.time())
+
+
+
+
+
+
+
+
+
+
+
+# ----
+
+
 
 # RIPS - estimates using the function
 run_estimations(rips_pbid, "n_hospitalizaciones", "df_hospitalizaciones")
 run_estimations(rips_pbid, "n_urgencias", "df_urgencias")
 
+
+# Ejemplo de uso:
+# Suponiendo que df_urgencias es tu conjunto de datos y deseas calcular las estimaciones para la variable n_hospitalizaciones
+run_estimations(df_urgencias, "n_hospitalizaciones", "cutoff30", "df_urgencias_estimations_cutoff30")
+
+# Puedes llamar a la función múltiples veces para diferentes variables y cutoffs
+run_estimations(df_urgencias, "n_urgencias", "cutoff40", "df_urgencias_estimations_cutoff40")
+run_estimations(df_urgencias, "n_hospitalizaciones", "cutoff21", "df_urgencias_estimations_cutoff21")
+
+# Para acceder a los resultados:
+# df_urgencias_estimations_cutoff30
+# df_urgencias_estimations_cutoff40
+# df_urgencias_estimations_cutoff21
 
 # PILA - estimates using the function
 
