7d4f9c496f9a952fdc4b3baae95acd3afe073d1e
Bspa7 <bpinedaareiza@worldbank.org>
2024-06-27 09:49:45 -0500

    Including controls - step 1

--- R/Exploratory analysis/Step3.1 - first estimations_rips - balanced.R
+++ R/Exploratory analysis/Step3.1 - first estimations_rips - balanced.R
@@ -1,4 +1,4 @@
-paquetes <- c("rddensity", "writexl", "arrow", "tidyverse", "haven", "dplyr", "openxlsx", "ggplot2", "rdrobust", "cowplot", "ragg", "R.utils")
+paquetes <- c("rddensity", "writexl", "arrow", "tidyverse", "haven", "dplyr", "openxlsx", "ggplot2", "rdrobust", "cowplot", "ragg", "R.utils", "tictoc")
 for (paquete in paquetes) {
   if (!require(paquete, character.only = TRUE)) {
     install.packages(paquete)
@@ -7,8 +7,8 @@ for (paquete in paquetes) {
 }
 
 
-base_dir <- "//wmedesrv/GAMMA/Christian Posso/_banrep_research/proyectos/project_transport_health"
-#base_dir  <- "D:/Steban Pineda/Documents/DIME/Transportation and health"
+#base_dir <- "//wmedesrv/GAMMA/Christian Posso/_banrep_research/proyectos/project_transport_health"
+base_dir  <- "D:/Steban Pineda/Documents/DIME/Transportation and health"
 project_folder <- base_dir
 data_dir       <- file.path(base_dir, "data")
 output_folder  <- file.path(base_dir, "outputs")
@@ -140,8 +140,9 @@ main_rips         <- open_dataset(RIPS_history_file) %>%
   left_join(
     open_dataset(sprintf("%s/%s", project_folder, "data/master.parquet")),
     by = 'personabasicaid'
-  ) %>% 
-  filter(!is.na(puntaje))
+  ) %>%
+    rename(COD_MPIO = COD_MPIO.x, COD_DPTO = COD_DPTO.x, puntaje = puntaje.x, sexo = sexo.x, estrato = estrato.x, nivel=nivel.x, grado=grado.x) %>% 
+    filter(!is.na(puntaje))
 
 #%>% 
 #  filter(!is.na(puntaje))
@@ -164,6 +165,7 @@ rips_pbid <- main_rips %>%
     year = year(DATE)
   )
 
+
 rips_pbid <- rips_pbid %>%
   filter(year>=2012 & year<=2019) %>% 
   mutate(
@@ -173,18 +175,67 @@ rips_pbid <- rips_pbid %>%
     estrato_3 = if_else(estrato == 3, 1, 0),
     estrato_4 = if_else(estrato == 4, 1, 0),
     estrato_5 = if_else(estrato == 5, 1, 0),
-    estrato_6 = if_else(estrato == 6, 1, 0)
+    estrato_6 = if_else(estrato == 6, 1, 0),
+    activi_sin = if_else(activi == 0, 1, 0),	
+    activi_tra = if_else(activi == 1, 1, 0),
+    activi_bus = if_else(activi == 2, 1, 0),
+    activi_est = if_else(activi == 3, 1, 0),
+    activi_hog = if_else(activi == 4, 1, 0),
+    estcivil_unlibr = if_else(estcivil == 1, 1, 0),
+    estcivil_casado = if_else(estcivil == 2, 1, 0),
+    estcivil_solter = if_else(estcivil == 4, 1, 0),
+    anios_educacion = case_when(
+      # Primaria
+      nivel == 3 & (grado == 0 | is.na(grado)) ~ 0,
+      nivel == 3 & grado == 1 ~ 1,
+      nivel == 3 & grado == 2 ~ 2,
+      nivel == 3 & grado == 3 ~ 3,
+      nivel == 3 & grado == 4 ~ 4,
+      nivel == 3 & grado == 5 ~ 5,
+      
+      # Secundaria
+      nivel == 2 & (grado == 0 | is.na(grado)) ~ 5,
+      nivel == 2 & (grado == 6 | grado == 1) ~ 6,
+      nivel == 2 & (grado == 7 | grado == 2) ~ 7,
+      nivel == 2 & (grado == 8 | grado == 3) ~ 8,
+      nivel == 2 & (grado == 9 | grado == 4) ~ 9,
+      nivel == 2 & (grado == 10 | grado == 5) ~ 10,
+      nivel == 2 & (grado == 11 | grado == 6) ~ 11,
+      
+      # Técnica o tecnología
+      nivel == 3 ~ 12,
+      
+      # Universitaria
+      nivel == 4 ~ 13,
+      
+      # Postgrado
+      nivel == 5 ~ 14,
+      
+      # Valor por defecto
+      TRUE ~ 0
+    )    
   ) %>% 
   group_by(quarterly_date, personabasicaid) %>%  
   summarise(
     puntaje   = max(puntaje, na.rm = TRUE),    
     genero    = max(genero,  na.rm = TRUE),
+    fechanto  = max(fechanto,  na.rm = TRUE),    
     estrato_1 = max(estrato_1, na.rm = TRUE),
     estrato_2 = max(estrato_2, na.rm = TRUE),
     estrato_3 = max(estrato_3, na.rm = TRUE),
     estrato_4 = max(estrato_4, na.rm = TRUE),
     estrato_5 = max(estrato_5, na.rm = TRUE),
     estrato_6 = max(estrato_6, na.rm = TRUE),
+    activi_sin = max(activi_sin, na.rm = TRUE),
+    activi_tra = max(activi_tra, na.rm = TRUE),
+    activi_bus = max(activi_bus, na.rm = TRUE),    
+    activi_est = max(activi_est, na.rm = TRUE),
+    activi_hog = max(activi_hog, na.rm = TRUE),
+    estcivil_unlibr = max(estcivil_unlibr, na.rm = TRUE),
+    estcivil_casado = max(estcivil_casado, na.rm = TRUE),    
+    estcivil_solter = max(estcivil_solter, na.rm = TRUE),
+    anios_educacion = max(anios_educacion, na.rm = TRUE),  
+    ingresos = max(ingresos, na.rm = TRUE),      
     edad_RIPS = max(edad_RIPS, na.rm = TRUE),
     n_visitas = n(),
     d_visitas = if_else(n() > 0, 1, 0),
@@ -219,13 +270,21 @@ rips_est <- rips_pbid %>%
   centrar_puntaje() %>% 
   collect
 
-
-#set.seed(123)
-#rips_est <- rips_est %>% 
-#  sample_frac(0.001)
+rips_est <- rips_est %>% 
+  sample_frac(0.005)
 
 rm(rips_pbid)
 gc()
+
+# -----------
+
+# PROVISIONAL SAMPLE ------------------------------------------------------------
+
+#write_parquet(rips_est, file.path(data_dir, "panel_prov_rips.parquet"))
+
+rips_est <- open_dataset(sprintf('%s/%s', data_dir, "panel_prov_rips.parquet")) %>%  collect
+
+# -----------
 
 # Skeleton with all individuals observed in all periods
 aux_time <- seq(as.Date("2012-01-01"), as.Date("2019-12-01"), by = "quarter")
@@ -239,11 +298,21 @@ rips_est_balanced <- merge(skeleton, rips_est, by = c("quarterly_date", "persona
 rm(rips_est, skeleton)
 gc()
 
+
 # Replacing NA's to zero in balance variables
 rips_est_balanced <- rips_est_balanced %>%
   group_by(personabasicaid) %>%
-  fill(puntaje, genero, estrato_1, estrato_2, estrato_3, estrato_4, estrato_5, estrato_6, cutoff30, cutoff40, .direction = "downup") %>%
-  ungroup()
+  fill(puntaje, genero, 
+       estrato_1, estrato_2, estrato_3, estrato_4, estrato_5, estrato_6, 
+       activi_sin, activi_tra, activi_bus, activi_est, activi_hog, 
+       estcivil_unlibr, estcivil_casado, estcivil_solter, anios_educacion,
+       cutoff30, cutoff40, fechanto, .direction = "downup") %>%
+  ungroup() %>%
+  mutate(across(c(puntaje, genero, 
+                  estrato_1, estrato_2, estrato_3, estrato_4, estrato_5, estrato_6, 
+                  activi_sin, activi_tra, activi_bus, activi_est, activi_hog, 
+                  estcivil_unlibr, estcivil_casado, estcivil_solter, anios_educacion,
+                  cutoff30, cutoff40), ~replace_na(., 0)))
 
 # Replacing NA's to zero in main variables
 rips_est_balanced <- rips_est_balanced %>%
@@ -256,7 +325,29 @@ rips_est_balanced <- rips_est_balanced %>%
                 ~ replace_na(., 0)))
 
 
+# Crear la variable edad_sisben
+rips_est_balanced <- rips_est_balanced %>%
+  mutate(edad_sisben = floor(interval(fechanto, quarterly_date) / years(1)))
+
+# Reordenar las columnas
+rips_est_balanced <- rips_est_balanced %>%
+  select(personabasicaid, quarterly_date, fechanto, edad_sisben, everything())
+
+
+
+
 # 5. Estimations ---------------------------------------------------------------
+
+# Changing the direction of the running variable | Interpretation
+rips_est_balanced <-  rips_est_balanced %>% 
+  mutate(cutoff30_origi = cutoff30,
+         cutoff40_origi = cutoff40,
+         cutoff30 = cutoff30*-1,
+         cutoff40 = cutoff40*-1)
+
+rips_est_balanced <- rips_est_balanced %>%
+  select(personabasicaid, cutoff30, cutoff30_origi, cutoff40, cutoff40_origi, quarterly_date, fechanto, edad_sisben, everything())
+
 
 # Using the cutoff=30
 run_estimations(rips_est_balanced, "n_visitas", "cutoff30", "rips30_visitas")
@@ -279,6 +370,25 @@ run_estimations(rips_est_balanced, "d_cardio", "cutoff30", "rips30_d_cardio")
 run_estimations(rips_est_balanced, "d_preven", "cutoff30", "rips30_d_preven")
 run_estimations(rips_est_balanced, "d_prenat", "cutoff30", "rips30_d_prenat")
 run_estimations(rips_est_balanced, "d_respir", "cutoff30", "rips30_d_respir")
+# Balance
+run_estimations(rips_est_balanced, "genero", "cutoff30", "rips30_genero")
+run_estimations(rips_est_balanced, "edad_sisben", "cutoff30", "rips30_edad_sisben")
+run_estimations(rips_est_balanced, "estrato_1", "cutoff30", "rips30_estrato_1")
+run_estimations(rips_est_balanced, "estrato_2", "cutoff30", "rips30_estrato_2")
+run_estimations(rips_est_balanced, "estrato_3", "cutoff30", "rips30_estrato_3")
+run_estimations(rips_est_balanced, "estrato_4", "cutoff30", "rips30_estrato_4")
+run_estimations(rips_est_balanced, "estrato_5", "cutoff30", "rips30_estrato_5")
+run_estimations(rips_est_balanced, "estrato_6", "cutoff30", "rips30_estrato_6")
+run_estimations(rips_est_balanced, "activi_sin", "cutoff30", "rips30_activi_sin")
+run_estimations(rips_est_balanced, "activi_tra", "cutoff30", "rips30_activi_tra")
+run_estimations(rips_est_balanced, "activi_bus", "cutoff30", "rips30_activi_bus")
+run_estimations(rips_est_balanced, "activi_est", "cutoff30", "rips30_activi_est")
+run_estimations(rips_est_balanced, "activi_hog", "cutoff30", "rips30_activi_hog")
+run_estimations(rips_est_balanced, "estcivil_unlibr", "cutoff30", "rips30_estcivil_unlibr")
+run_estimations(rips_est_balanced, "estcivil_casado", "cutoff30", "rips30_estcivil_casado")
+run_estimations(rips_est_balanced, "estcivil_solter", "cutoff30", "rips30_estcivil_solter")
+run_estimations(rips_est_balanced, "anios_educacion", "cutoff30", "rips30_anios_educacion")
+run_estimations(rips_est_balanced, "ingresos", "cutoff30", "rips30_ingresos")
 
 # Using the cutoff=40
 run_estimations(rips_est_balanced, "n_visitas", "cutoff40", "rips40_visitas")
@@ -301,22 +411,30 @@ run_estimations(rips_est_balanced, "d_cardio", "cutoff40", "rips40_d_cardio")
 run_estimations(rips_est_balanced, "d_preven", "cutoff40", "rips40_d_preven")
 run_estimations(rips_est_balanced, "d_prenat", "cutoff40", "rips40_d_prenat")
 run_estimations(rips_est_balanced, "d_respir", "cutoff40", "rips40_d_respir")
+# Balance
+run_estimations(rips_est_balanced, "genero", "cutoff40", "rips40_genero")
+run_estimations(rips_est_balanced, "edad_sisben", "cutoff40", "rips40_edad_sisben")
+run_estimations(rips_est_balanced, "estrato_1", "cutoff40", "rips40_estrato_1")
+run_estimations(rips_est_balanced, "estrato_2", "cutoff40", "rips40_estrato_2")
+run_estimations(rips_est_balanced, "estrato_3", "cutoff40", "rips40_estrato_3")
+run_estimations(rips_est_balanced, "estrato_4", "cutoff40", "rips40_estrato_4")
+run_estimations(rips_est_balanced, "estrato_5", "cutoff40", "rips40_estrato_5")
+run_estimations(rips_est_balanced, "estrato_6", "cutoff40", "rips40_estrato_6")
+run_estimations(rips_est_balanced, "activi_sin", "cutoff40", "rips40_activi_sin")
+run_estimations(rips_est_balanced, "activi_tra", "cutoff40", "rips40_activi_tra")
+run_estimations(rips_est_balanced, "activi_bus", "cutoff40", "rips40_activi_bus")
+run_estimations(rips_est_balanced, "activi_est", "cutoff40", "rips40_activi_est")
+run_estimations(rips_est_balanced, "activi_hog", "cutoff40", "rips40_activi_hog")
+run_estimations(rips_est_balanced, "estcivil_unlibr", "cutoff40", "rips40_estcivil_unlibr")
+run_estimations(rips_est_balanced, "estcivil_casado", "cutoff40", "rips40_estcivil_casado")
+run_estimations(rips_est_balanced, "estcivil_solter", "cutoff40", "rips40_estcivil_solter")
+run_estimations(rips_est_balanced, "anios_educacion", "cutoff40", "rips40_anios_educacion")
+run_estimations(rips_est_balanced, "ingresos", "cutoff40", "rips40_ingresos")
 
 
 # Data frame lists
-list_est_rips30 <- list(
-  rips30_visitas, rips30_c_prev, rips30_c_pren, rips30_c_cancer, rips30_c_cardio, rips30_c_respir, 
-  rips30_consultas, rips30_hosp, rips30_proc, rips30_urgencias,
-  rips30_d_visitas, rips30_d_consultas, rips30_d_hosp, rips30_d_proc, rips30_d_urgencias,
-  rips30_d_cancer, rips30_d_cardio, rips30_d_preven, rips30_d_prenat, rips30_d_respir
-)
-
-list_est_rips40 <- list(
-  rips40_visitas, rips40_c_prev, rips40_c_pren, rips40_c_cancer, rips40_c_cardio, rips40_c_respir, 
-  rips40_consultas, rips40_hosp, rips40_proc, rips40_urgencias,
-  rips40_d_visitas, rips40_d_consultas, rips40_d_hosp, rips40_d_proc, rips40_d_urgencias,
-  rips40_d_cancer, rips40_d_cardio, rips40_d_preven, rips40_d_prenat, rips40_d_respir
-)
+list_est_rips30 <- mget(ls(pattern = "^rips30_"))
+list_est_rips40 <- mget(ls(pattern = "^rips40_"))
 
 
 # All dataframes to one
@@ -324,10 +442,16 @@ est_rips30 <- bind_rows(list_est_rips30)
 est_rips40 <- bind_rows(list_est_rips40)
 
 # Save dataframe in .parquet format
-write_parquet(est_rips30, file.path(output_folder, "parquet/20240620-est_rips30.parquet"))
-write_parquet(est_rips40, file.path(output_folder, "parquet/20240620-est_rips40.parquet"))
+write_parquet(est_rips30, file.path(output_folder, "parquet/20240627-est_rips30.parquet"))
+write_parquet(est_rips40, file.path(output_folder, "parquet/20240627-est_rips40.parquet"))
 
 # Elements list
+estimaciones_list_objects <- c(
+  ls(pattern = "^rips30_"),
+  ls(pattern = "^rips40_")
+)
+
+
 estimaciones_list_objects <- c(
   "rips30_visitas", "rips30_c_prev", "rips30_c_pren", "rips30_c_cancer", "rips30_c_cardio", "rips30_c_respir", 
   "rips30_consultas", "rips30_hosp", "rips30_proc", "rips30_urgencias", "rips30_d_visitas", "rips30_d_consultas",
@@ -341,8 +465,8 @@ estimaciones_list_objects <- c(
 # Drop some elements in the environment
 rm(list = estimaciones_list_objects, envir = .GlobalEnv)
 
-
-
+  
+rdplot(y=rips_est_balanced$estrato_1, x=rips_est_balanced$cutoff30)
 
 
 
